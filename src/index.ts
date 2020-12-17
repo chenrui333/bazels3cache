b843de84a106b365fc09d7ab9aecb373cb542355
Rui Chen <rui@meetup.com>
2020-12-16 21:10:49 -0500

    remove daemon step for docker

--- src/index.ts
+++ src/index.ts
@@ -63,57 +63,27 @@ function daemonMain(args: Args, onDoneInitializing: () => void) {
 function main(args: string[]) {
     const DONE_INITIALIZING = "done_initializing";
 
-    // When bazelcache launches, it spawns a child bazelcache with "--daemon"
-    // added to the command line.
-    //
-    // The parent process then waits until that child process either exits, or sends
-    // us a "done_initializing" message. Then the parent process exits.
-    if (args.indexOf("--daemon") === -1) {
-        // We are parent process
-        const devnull = fs.openSync("/dev/null", "r+");
-        // As described here https://github.com/nodejs/node/issues/17592, although
-        // child_process.fork() doesn't officially support `detached: true`, it works
-        const child = child_process.fork(__filename, ["--daemon"].concat(args),
-            <any>{ detached: true });
+    // child process
+    daemonMain(minimist<Args>(args), () => {
+        // Now that the daemon has finished initializing, we need to:
+        // - close stdin, stdout, and stderr, so that we don't keep these handles
+        //   open cause problems
+        // - open /dev/null for all three of those
+        // - send a "done_initializing" message to our parent process.
 
-        // This is so that if we terminate *without* receiving a "done_initializig"
-        // message from the child process, that's because the child process
-        // terminated unexpectedly, so we should exit with an error code.
-        //
-        // While we're waiting, the child process still has stdout and stderr, and
-        // can send any messages to there.
-        process.exitCode = 1;
+        fs.closeSync(0);
+        fs.closeSync(1);
+        fs.closeSync(2);
+        // Odd: If I don't do the following, then sometimes writes such as
+        // console.log() still show up on the screen. I don't get it.
+        process.stdout.write = process.stderr.write = (): undefined => undefined;
+        fs.openSync("/dev/null", "r+"); // stdin
+        fs.openSync("/dev/null", "r+"); // stdout
+        fs.openSync("/dev/null", "r+"); // stderr
 
-        child.on("message", msg => {
-            if (msg === DONE_INITIALIZING) {
-                child.unref(); // don't wait for the child process to terminate
-                child.disconnect(); // don't wait on the ipc channel any more
-                process.exitCode = 0; // now we can exit cleanly
-            }
-        });
-    } else {
-        // child process
-        daemonMain(minimist<Args>(args), () => {
-            // Now that the daemon has finished initializing, we need to:
-            // - close stdin, stdout, and stderr, so that we don't keep these handles
-            //   open cause problems
-            // - open /dev/null for all three of those
-            // - send a "done_initializing" message to our parent process.
-
-            fs.closeSync(0);
-            fs.closeSync(1);
-            fs.closeSync(2);
-            // Odd: If I don't do the following, then sometimes writes such as
-            // console.log() still show up on the screen. I don't get it.
-            process.stdout.write = process.stderr.write = (): undefined => undefined;
-            fs.openSync("/dev/null", "r+"); // stdin
-            fs.openSync("/dev/null", "r+"); // stdout
-            fs.openSync("/dev/null", "r+"); // stderr
-
-            // Tells the parent that we initialized successfully and it can exit
-            process.send(DONE_INITIALIZING);
-        });
-    }
+        // Tells the parent that we initialized successfully and it can exit
+        process.send(DONE_INITIALIZING);
+    });
 }
 
 main(process.argv.slice(2));
